2624e69fe59b4d660136add5da6ebf7cb4f2a5fc
intansepriyelmi <153574546+intansepriyelmi@users.noreply.github.com>
2023-12-12 09:42:37 +0700

    Create kalkulator.c
    tugas terakhir pertemuan 15 algoritma pemograman

---
+++ kalkulator.c
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int a,b,tambah,kali,kurang,bagi;
+	
+cout<<"============="<<endl;
+cout<<"PROGRAM KALKULATOR INTAN SEPRIYELMI"<<endl;
+cout<<"============="<<endl;
+
+cout<<"MASUKKAN NILAI A : ";cin>>a;
+cout<<"MASUKKAN NILAI B : ";cin>>b;
+
+
+tambah = a + b;
+kali   = a * b;
+kurang = a - b;
+bagi   = a / b;
+
+
+cout<<"==============="<<endl;
+cout<<"HASIL DARI TAMBAH : "<<tambah<<endl;
+cout<<"HASIL DARI KALI   : "<<kali<<endl;
+cout<<"HASIL DARI KURANG : "<<kurang<<endl;
+cout<<"HASIL DARI BAGI   : "<<bagi<<endl;
+
+
+return 0;
+
+}
